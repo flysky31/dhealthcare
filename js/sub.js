8d8c9d1bd00e3dd2c5c7bd8672fc3c6a0ed3d58f
LeeJaMin <sweetmin33@naver.com>
2024-08-14 00:21:21 +0900

    Update sub.js

--- js/sub.js
+++ js/sub.js
@@ -11,31 +11,93 @@ $(document).ready(function() {
     });
 
 
-    $("#encyclical01 ul li").click(function(){
-        $(this).toggleClass("on");
-        $('#encyclical01 ul li').not(this).removeClass('on');   
-        $("#encyclical01 ul li .revers").removeClass("on");
-      
-     
-        let subElement = $(this).find(".content");
-
-        if (subElement.is(":visible")) {
-          subElement.slideUp();
-          
-          
-        } else {
-          $("#encyclical01 ul li .content").slideUp();
-          subElement.slideDown();
-        }
-
-        if($(this).hasClass("on")){
-            //$(this).find(".revers > span").html("접어두기");            
-            $(this).find(".revers").addClass("on");
-          } else {
-            $//(this).find(".revers > span").html("펼쳐보기");
-            $(this).find(".revers").removeClass("on");
-        }
-        
-       
-    });
 });
+
+
+
+$(document).ready(function(){
+    function toggleDropdown() {
+        // Check if the screen width is below 1024 pixels
+        // if (window.innerWidth < 1024) {
+        //     // $("#bo-cate-ul").toggleClass("on");
+        //     $("#bo-cate-ul").addClass("on");
+        //     $("#bo-cate h2").click(function(){
+        //         $(this).toggleClass("on");
+       
+        //     });
+        // } else {
+        //     $("#bo-cate-ul").removeClass("on");
+        // }
+    }
+
+    $("#bo-cate h2").click(function(){
+        $(this).toggleClass("on");
+        $("#bo-cate-ul").toggleClass("on");
+    });
+
+    // Check window width on resize
+    $(window).resize(function(){
+        toggleDropdown();
+    });
+
+
+
+      //상세슬라이드
+  var swiper1 = new Swiper(".mySwiper1", {
+    //loop: true,
+    spaceBetween: 20,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      540: {
+            slidesPerView: 2,  
+            spaceBetween: 20     
+            },
+
+      1360: {
+        slidesPerView: 3,        
+        spaceBetween: 20 
+      },
+    },
+  });
+    var swiper2 = new Swiper(".mySwiper2", {
+        //loop: true,
+        //spaceBetween: 10,
+
+        navigation: {
+          nextEl: ".swiper_arrow.swiper-button-next",
+          prevEl: ".swiper_arrow.swiper-button-prev",
+        },
+        //effect:"fade",
+        thumbs: {
+          swiper: swiper1,
+        },
+      });
+      
+      
+
+    
+});
+
+
+
+
+$(document).ready(function() {
+    $(".tablinks").click(function() {
+      var tabId = $(this).data("tab");
+
+      // 모든 탭 숨기기
+      $(".tabcontent").hide();
+      // 모든 탭 버튼 클래스 초기화
+      $(".tablinks").removeClass("active");
+
+      // 선택한 탭 보이기
+      $("#" + tabId).show();
+      // 선택한 탭 버튼에 active 클래스 추가
+      $(this).addClass("active");
+    });
+
+    // 페이지 로드시 기본으로 열릴 탭 설정
+    $(".tablinks[data-tab='Tab1']").click();
+  });

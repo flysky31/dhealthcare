39192e2de231f7b23fcf60c311d1ce6faf0bf899
LeeJaMin <sweetmin33@naver.com>
2024-08-18 18:47:10 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
+//animation
 function aniCtrl(){
-    var _st = $(window).scrollTop();
-   
+    var _st = $(window).scrollTop();   
     var _wH = $(window).height();
 
     $('.ani-item').each(function(){
@@ -13,11 +13,32 @@ function aniCtrl(){
 
 
 $(document).ready(function(){
-
     aniCtrl();
-
     $(window).scroll(function(){
         aniCtrl();
     });
 });
 
+
+//board tab
+$(document).ready(function () {
+    // 탭 클릭 이벤트
+    $('.tab').click(function (event) {
+        event.preventDefault();
+
+        $('.tab').removeClass('active');
+        $('.tab-pane').removeClass('active');
+        
+        $(this).addClass('active');
+        var tabId = $(this).data('tab');
+
+        $('#' + tabId).addClass('active');
+
+        history.replaceState(null, null, '#' + tabId);
+    });
+
+    if (window.location.hash) {
+        var tabId = window.location.hash.replace('#', '');
+        $('.tab[data-tab="' + tabId + '"]').click();
+    }
+});
